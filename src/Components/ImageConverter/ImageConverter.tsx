d70a58276b41836e5533ba6692041ada2f7965b9
Mats Grøsvik <mats.grosvik@knowit.no>
2025-11-12 18:07:16 +0100

    lint

--- src/Components/ImageConverter/ImageConverter.tsx
+++ src/Components/ImageConverter/ImageConverter.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./ImageConverter.scss";
 import Panel from "../Panel/Panel";
 
 const ImageConverter: React.FC = () => {
   const [svgContent, setSvgContent] = useState<string>("");
-  const [size, setSize] = useState(9);
-  const [sensetivity, setSensetivity] = useState(20);
+  const [size, setSize] = useState(3);
+  const [sensetivity, setSensetivity] = useState(0);
   const [brightness, setBrightness] = useState(0.45);
   const [inverted, setInverted] = useState(false);
-  const [color, setColor] = useState("#1b1f32");
+  const [color, setColor] = useState("#45a6ff");
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [maxImageSize, setMaxImageSize] = useState(800);
   const [fileName, setFilename] = useState("");
@@ -17,90 +17,104 @@ const ImageConverter: React.FC = () => {
   const greenColor = "#61c55d";
   const pineColor = "#ca9462";
   const whiteColor = "#fffffa";
-  const processImage = (img: HTMLImageElement) => {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d")!;
-    let width = img.width;
-    let height = img.height;
-    const aspectRatio = width / height;
 
-    width = maxImageSize;
-    height = width / aspectRatio;
+  const processImage = useCallback(
+    (img: HTMLImageElement) => {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d")!;
+      let width = img.width;
+      let height = img.height;
+      const aspectRatio = width / height;
 
-    canvas.width = width;
-    canvas.height = height;
-    ctx.drawImage(img, 0, 0, width, height);
+      width = maxImageSize;
+      height = width / aspectRatio;
 
-    const imageData = ctx.getImageData(0, 0, width, height);
-    const pixels = imageData.data;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(img, 0, 0, width, height);
 
-    let svgContent = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">`;
-    svgContent += `<rect width="100%" height="100%" fill="none"/>`;
+      const imageData = ctx.getImageData(0, 0, width, height);
+      const pixels = imageData.data;
 
-    const hexHeight = size;
-    const hexWidth = size;
+      let svgContent = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">`;
+      svgContent += `<rect width="100%" height="100%" fill="none"/>`;
 
-    for (let y = 0; y < height + hexHeight; y += hexHeight) {
-      for (let x = hexWidth / 2; x < width + hexWidth; x += hexWidth) {
-        const centerX = Math.floor(x);
-        const centerY = Math.floor(y);
+      const hexHeight = size;
+      const hexWidth = size;
 
-        if (
-          centerX >= 0 &&
-          centerX < width &&
-          centerY >= 0 &&
-          centerY < height
-        ) {
-          const index = (centerY * width + centerX) * 4;
-          const r = pixels[index];
-          const g = pixels[index + 1];
-          const b = pixels[index + 2];
-          const a = pixels[index + 3];
-          // const rgb = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-          const rgb = (r + g + b) / 3;
+      for (let y = 0; y < height + hexHeight; y += hexHeight) {
+        for (let x = hexWidth / 2; x < width + hexWidth; x += hexWidth) {
+          const centerX = Math.floor(x);
+          const centerY = Math.floor(y);
 
-          // If alpha is 0 (fully transparent) or pixel is completely white, skip drawing the circle
-          if (a === 0 || (r === 255 && g === 255 && b === 255)) {
-            continue;
-          }
-          if (!inverted) {
-          }
+          if (
+            centerX >= 0 &&
+            centerX < width &&
+            centerY >= 0 &&
+            centerY < height
+          ) {
+            const index = (centerY * width + centerX) * 4;
+            const r = pixels[index];
+            const g = pixels[index + 1];
+            const b = pixels[index + 2];
+            const a = pixels[index + 3];
+            // const rgb = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            const rgb = (r + g + b) / 3;
 
-          if (rgb < 255 - sensetivity && rgb > 35) {
-            // Assign color based on brightness, skip darkest colors
-            let fillColor = whiteColor;
-            const t1 = 44;
-            const t2 = 83;
-            const t3 = 160;
-
-            if (rgb < t1) {
-              fillColor = greenColor;
-            } else if (rgb < t2) {
-              fillColor = blueColor;
-            } else if (rgb < t3) {
-              fillColor = pineColor;
-            } else {
-              fillColor = whiteColor;
+            // If alpha is 0 (fully transparent) or pixel is completely white, skip drawing the circle
+            if (a === 0 || (r === 255 && g === 255 && b === 255)) {
+              continue;
+            }
+            if (!inverted) {
             }
 
-            // Invert colors if inverted is true
-            if (inverted) {
-              if (fillColor === greenColor) fillColor = whiteColor;
-              else if (fillColor === blueColor) fillColor = pineColor;
-              else if (fillColor === pineColor) fillColor = blueColor;
-              else if (fillColor === whiteColor) fillColor = greenColor;
-            }
+            if (rgb < 255 - sensetivity && rgb > 35) {
+              // Assign color based on brightness, skip darkest colors
+              let fillColor = whiteColor;
+              const t1 = 44;
+              const t2 = 83;
+              const t3 = 160;
 
-            const radius = size * brightness;
-            svgContent += `<circle cx="${centerX}" cy="${centerY}" r="${radius}" fill="${fillColor}" />`;
+              if (rgb < t1) {
+                fillColor = greenColor;
+              } else if (rgb < t2) {
+                fillColor = blueColor;
+              } else if (rgb < t3) {
+                fillColor = pineColor;
+              } else {
+                fillColor = whiteColor;
+              }
+
+              // Invert colors if inverted is true
+              if (inverted) {
+                if (fillColor === greenColor) fillColor = whiteColor;
+                else if (fillColor === blueColor) fillColor = pineColor;
+                else if (fillColor === pineColor) fillColor = blueColor;
+                else if (fillColor === whiteColor) fillColor = greenColor;
+              }
+
+              const radius = size * brightness;
+              svgContent += `<circle cx="${centerX}" cy="${centerY}" r="${radius}" fill="${fillColor}" />`;
+            }
           }
         }
       }
-    }
 
-    svgContent += "</svg>";
-    setSvgContent(svgContent);
-  };
+      svgContent += "</svg>";
+      setSvgContent(svgContent);
+    },
+    [
+      size,
+      sensetivity,
+      brightness,
+      inverted,
+      maxImageSize,
+      blueColor,
+      greenColor,
+      pineColor,
+      whiteColor,
+    ]
+  );
 
   useEffect(() => {
     if (imageFile) {
@@ -114,7 +128,7 @@ const ImageConverter: React.FC = () => {
       let name = imageFile.name.split(".");
       setFilename(name[0]);
     }
-  }, [imageFile, size, inverted, color, maxImageSize, sensetivity, brightness]);
+  }, [imageFile, processImage]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
